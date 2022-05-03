0c87fb952687fc115b2b85630fd90275b3cc048b
Claudia de Oliveira Ramos <cmplg20@gmail.com>
2022-05-03 12:47:36 -0300

    Substituindo Setters e Getters por @Setter e @Getter em CartItem.java

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -2,6 +2,8 @@ package org.agoncal.application.petstore.domain;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -9,6 +11,8 @@ import javax.validation.constraints.NotNull;
  *         --
  */
 
+@Getter
+@Setter
 public class CartItem {
 
     // ======================================
@@ -36,26 +40,6 @@ public class CartItem {
 
     public Float getSubTotal() {
         return item.getUnitCost() * quantity;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Item getItem() {
-        return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
     }
 
     // ======================================
