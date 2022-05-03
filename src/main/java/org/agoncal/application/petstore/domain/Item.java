a2fb791cba43a4b98093385d5661e3729e158ea1
Claudia de Oliveira Ramos <cmplg20@gmail.com>
2022-05-03 12:33:20 -0300

    Adicionando dependencia Lombok e substituindo Getters por @Getter

--- src/main/java/org/agoncal/application/petstore/domain/Item.java
+++ src/main/java/org/agoncal/application/petstore/domain/Item.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
+import lombok.Getter;
 
 /**
  * @author Antonio Goncalves
@@ -15,6 +16,7 @@ import javax.xml.bind.annotation.XmlTransient;
  *         --
  */
 
+@Getter
 @Entity
 @NamedQueries({
         @NamedQuery(name = Item.FIND_BY_PRODUCT_ID, query = "SELECT i FROM Item i WHERE i.product.id = :productId"),
@@ -74,44 +76,21 @@ public class Item {
     // =         Getters & setters          =
     // ======================================
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Float getUnitCost() {
-        return unitCost;
     }
 
     public void setUnitCost(Float unitCost) {
         this.unitCost = unitCost;
     }
 
-    public String getImagePath() {
-        return imagePath;
-    }
-
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
 
-    public Product getProduct() {
-        return product;
-    }
-
     public void setProduct(Product product) {
         this.product = product;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setDescription(String description) {
